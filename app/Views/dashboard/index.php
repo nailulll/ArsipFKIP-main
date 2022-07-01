df240d5dcee1573e29cda981e543a96d67a03fc6
Nailul Ngafwa <83789227+nailulll@users.noreply.github.com>
2022-07-01 15:45:46 +0700

    konfigurasi adminlte 3

---
+++ app/Views/dashboard/index.php
@@ -0,0 +1,6 @@
+<?= $this->extend('layout/template') ?>
+<?= $this->section('content') ?>
+<div class="row">
+    <h1>Hello World</h1>
+</div>
+<?= $this->endSection() ?>
---
+++ app/Views/login.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="/assets/dist/css/adminlte.min.css">
+    <title>Login Form</title>
+</head>
+
+<body>
+    <div class="hold-transition login-page">
+        <div class="login-box">
+            <div class="login-logo">
+                <h2>Logo Here</h2>
+            </div>
+            <!-- /.login-logo -->
+            <div class="card">
+                <div class="card-body login-card-body">
+                    <p class="login-box-msg">Sign in to start your session</p>
+
+                    <form action="/home/dashboard/" method="post">
+                        <div class="input-group mb-3">
+                            <input type="email" class="form-control" placeholder="Email">
+                            <div class="input-group-append">
+                                <div class="input-group-text">
+                                    <span class="fas fa-envelope"></span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="input-group mb-3">
+                            <input type="password" class="form-control" placeholder="Password">
+                            <div class="input-group-append">
+                                <div class="input-group-text">
+                                    <span class="fas fa-lock"></span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-8">
+                                <div class="icheck-primary">
+                                    <input type="checkbox" id="remember">
+                                    <label for="remember">
+                                        Remember Me
+                                    </label>
+                                </div>
+                            </div>
+                            <!-- /.col -->
+                            <div class="col-4">
+                                <button type="submit" class="btn btn-dark btn-block">Sign In</button>
+                            </div>
+                            <!-- /.col -->
+                        </div>
+                    </form>
+                </div>
+                <!-- /.login-card-body -->
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
